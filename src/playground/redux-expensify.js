c8a2b35e6ff891bb05fb6c403e3ececf8342927c
John Knotts <jhnnyk@gmail.com>
2017-12-01 14:11:58 -0700

    es6 spread operator in reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,9 +1,33 @@
 import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
+
+// Actions
+const addExpense = (
+  { description = "", note = "", amount = 0, createdAt = 0 } = {}
+) => ({
+  type: "ADD_EXPENSE",
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+const removeExpense = ({ id } = {}) => ({
+  type: "REMOVE_EXPENSE",
+  id
+});
 
 // Expenses Reducer
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case "ADD_EXPENSE":
+      return [...state, action.expense];
+    case "REMOVE_EXPENSE":
+      return state.filter(exp => exp.id !== action.id);
     default:
       return state;
   }
@@ -31,7 +55,18 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(
+  addExpense({ description: "Rent", amount: 100 })
+);
+const expenseTwo = store.dispatch(
+  addExpense({ description: "Coffee", amount: 300 })
+);
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 const demoState = {
   expenses: [
