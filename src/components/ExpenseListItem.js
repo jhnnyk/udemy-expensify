4b71e6bf42b1c5ff05d0d507b259661befa61fd1
John Knotts <jhnnyk@gmail.com>
2017-12-23 17:14:03 -0700

    controlled inputs for filters

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = props => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
-    <h3>{props.description}</h3>
+    <h3>{description}</h3>
     <p>
-      {props.amount} - {props.createdAt}
+      {amount} - {createdAt}
     </p>
+    <button
+      onClick={() => {
+        dispatch(removeExpense({ id }));
+      }}
+    >
+      Remove
+    </button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
